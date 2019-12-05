f510654f10864eaf2b32269a800ed3bce1fd19b0
Dominik Trusinski <dominik.trusinski@gmail.com>
2019-12-05 21:16:32 +0100

    Move api url to env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://recipes-app-apsi.herokuapp.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://recipes-app-apsi.herokuapp.com',
 };
 
 /*
