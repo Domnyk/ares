f9e282ea87fcaa6605ef49133bcc8612a424d21c
Dominik Trusinski <dominik.trusinski@gmail.com>
2019-12-06 21:17:16 +0100

    Allow access to app only for logged users
    If user is not logged in then redirect to login page
    
    Tests for AuthGuard emit warnings about navigation oustide of Angular zone.
    It is cause by Angular bug.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,16 +5,15 @@ import {MyProfileComponent} from './my-profile/my-profile.component';
 import {SearchTabComponent} from './search-tab/search-tab.component';
 import {SearchResolver} from './search-tab/resolver/search-resolver';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { AuthGuard } from './route-guards/auth.guard';
 
 const routes: Routes = [
-  {path: 'main', component: MainPageComponent},
-  {path: 'profile', component: MyProfileComponent},
-  {
-    path: 'search', component: SearchTabComponent, resolve: {foundRecipes: SearchResolver}
-  }
-  ,
+  { path: '', canActivateChild: [AuthGuard], children: [
+    { path: 'main', component: MainPageComponent },
+    { path: 'profile', component: MyProfileComponent },
+    { path: 'search', component: SearchTabComponent, resolve: { foundRecipes: SearchResolver } },
+  ]},
   { path: 'login', component: LoginFormComponent },
-
 ];
 
 @NgModule({
