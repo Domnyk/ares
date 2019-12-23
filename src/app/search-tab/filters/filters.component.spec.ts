10fd5dacd54bbcf0b1deb5a761cc7fcb011f01d7
deszczkarolina <deszcz.karolina@gmail.com>
2019-12-23 13:26:36 +0100

    unit tests fixes

--- src/app/search-tab/filters/filters.component.spec.ts
+++ src/app/search-tab/filters/filters.component.spec.ts
@@ -1,6 +1,24 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { FiltersComponent } from './filters.component';
+import {FiltersComponent} from './filters.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {ElementBoxComponent} from './element-box/element-box.component';
+import {TranslateModule} from '@ngx-translate/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {Observable, timer} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {HttpClient, HttpHandler} from '@angular/common/http';
+import {RouterTestingModule} from '@angular/router/testing';
+
+
+const authDelay = 2000;
+const authService = {
+  login(u, p): Observable<boolean> {
+    return timer(authDelay).pipe(
+      map(_ => true)
+    );
+  }
+};
 
 describe('FiltersComponent', () => {
   let component: FiltersComponent;
@@ -8,10 +26,12 @@ describe('FiltersComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FiltersComponent ]
-    })
-    .compileComponents();
+      declarations: [FiltersComponent, ElementBoxComponent],
+      imports: [[TranslateModule.forRoot(), NgbModule, ReactiveFormsModule, RouterTestingModule]],
+      providers: [HttpClient, HttpHandler]
+    }).compileComponents();
   }));
+
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FiltersComponent);
@@ -22,4 +42,5 @@ describe('FiltersComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+})
+;
--- src/app/service/dictionary.service.spec.ts
+++ src/app/service/dictionary.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DictionaryService } from './dictionary.service';
+import {HttpClient, HttpHandler} from '@angular/common/http';
 
 describe('DictionaryService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [HttpClient, HttpHandler]
+  }));
 
   it('should be created', () => {
     const service: DictionaryService = TestBed.get(DictionaryService);
