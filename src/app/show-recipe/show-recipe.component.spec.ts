e08b16f95da36f6bb67cd9db1cc7d9bc46a7e30e
Dominik Trusinski <dominik.trusinski@gmail.com>
2020-01-13 14:40:31 +0100

    Add to favourites

--- src/app/show-recipe/show-recipe.component.spec.ts
+++ src/app/show-recipe/show-recipe.component.spec.ts
@@ -57,11 +57,15 @@ class MockActivatedRouteWithoutRecipe extends ActivatedRoute {
 describe('ShowRecipeComponent', () => {
   const recipeService = {
     findRecipeById: (id: number) => of(mockRecipe),
+<<<<<<< HEAD
     fetchRating: (recipeId: number, username: string) => of(mockRating),
   };
 
   const authService = {
     currentUser: of(mockCurrentUser),
+=======
+    fetchFavourites: () => of([0, 1, 2, 3]),
+>>>>>>> Add to favourites
   };
 
   const compileShowRecipeComponent = (activatedRoute: any) => {
